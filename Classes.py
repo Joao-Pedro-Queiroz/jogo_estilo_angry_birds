bf2a4d191b7982622f867312b895ec650f54cb22
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-08-13 12:28:12 -0300

    classe Jogo

--- Classes.py
+++ Classes.py
@@ -32,3 +32,34 @@ class TelaInicial:
         window.blit(texto_inicio, (100, 300)) # Desenha a imagem já carregada por pygame.image.load em window na posição (x, y).
 
         pygame.display.update()
+
+
+class Jogo:
+    def __init__(self):
+        pygame.init()
+        self.largura_jogo = 550
+        self.altura_jogo = 600
+        self.fonte_padrao = pygame.font.get_default_font() # Carrega a fonte padrão
+        self.largura_nave = 60
+        self.altura_nave = 50
+        self.window = pygame.display.set_mode((self.largura_jogo, self.altura_jogo))
+        pygame.display.set_caption('Space Atari') # Define o título da janela
+        self.indice_tela_atual = 0
+        self.telas = [TelaInicial(self.largura_jogo, self.altura_jogo, self.fonte_padrao)]
+
+    def game_loop(self):
+        tela_atual = self.telas[self.indice_tela_atual]
+
+        rodando = True
+        while rodando:
+            self.indice_tela_atual = tela_atual.atualiza_estado()
+
+            if self.indice_tela_atual == -1:
+                rodando = False
+            elif self.indice_tela_atual == 1:
+                tela_atual = self.telas[self.indice_tela_atual]
+                tela_atual.desenha(self.window)
+            else:
+                tela_atual = self.telas[self.indice_tela_atual]
+                tela_atual.desenha(self.window)
+                pygame.mixer.music.pause()
