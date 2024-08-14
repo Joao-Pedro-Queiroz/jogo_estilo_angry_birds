7a7c9ace4358a8a9b0390485689d5eef6f3b2c2c
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-08-14 14:53:02 -0300

    Classe telaJogo

--- jogo_estilo_angry_birds/Classes.py
+++ jogo_estilo_angry_birds/Classes.py
@@ -43,45 +43,38 @@ class Atrator:
         window.blit(nave, self.posicao) # Desenha a imagem já carregada por pygame.image.load em window na posição (x, y).
 
 class Bolinha():
-    def __init__(self,s0,v0,quantidade,torre,tamanho):
+    def __init__(self,s0,v0,posicao_torre,tamanho):
         self.imagem = pygame.image.load('assets/img/bola+canhao.png')
-        self.posicoes = []
-        self.velocidades = []
-        self.s0 = s0
-        self.v0=v0
-        self.torre=torre
+        self.s0 = np.array(s0)
+        self.v0 = np.array(v0)
+        self.torre=np.array(posicao_torre)
         self.tamanho=tamanho
-        self.quantidade=quantidade
-        for i in range(quantidade):
-            self.posicoes.append(self.s0)
-            self.velocidades.append(self.v0)
-        for i in range(len(self.velocidades)):
-            p = np.random.rand()
-            self.velocidades[i]=self.elocidades[i]*(p+1) 
+        self.posicoes = self.s0
+        self.velocidade = self.v0
+
     
-    def atualiza_estado(self,posicoes,velocidades,torre,aceleracao):
-            for i in range(len(posicoes)):
-                if posicoes[i][0]<10 or posicoes[i][0]>390 or posicoes[i][1]<10 or posicoes[i][1]>390: # Se eu chegar ao limite da tela, reinicio a posição do personagem
-                    posicoes[i]= self.s0
-                    velocidades[i] = torre-self.v0
-                    velocidades[i]= velocidades[i]*0.05
-                else:
-                    velocidades[i]= velocidades[i] + aceleracao
-                    posicoes[i] = posicoes[i] +  velocidades[i]
-    
+    def atualiza_estado(self,torre,aceleracao):
+            if self.posicoes[0]<10 or self.posicoes[0]>390 or self.posicoes[1]<10 or self.posicoes[1]>390: # Se eu chegar ao limite da tela, reinicio a posição do personagem
+                self.posicoes= self.s0
+                self.velocidades = torre-self.v0
+                self.velocidades= self.velocidades*0.05
+            else:
+                self.velocidades= self.velocidades + aceleracao
+                self.posicoes = self.posicoes + self.velocidades
+
     def desenha(self,window):
-        for i in range(self.quantidade):
-            bolinha = pygame.transform.scale(self.imagem, self.tamanho) # Redefinir dimensão da imagem
-            window.blit(bolinha, self.posicoes[i]) # Desenha a imagem já carregada por pygame.image.load em window na posição (x, y).
+        bolinha = pygame.transform.scale(self.imagem, self.tamanho) # Redefinir dimensão da imagem
+        window.blit(bolinha, self.posicoes) # Desenha a imagem já carregada por pygame.image.load em window na posição (x, y).
 
 
 class TelaInicial:
-    def __init__(self, largura_jogo, altura_jogo, fonte_padrao):
+    def __init__(self, largura_jogo, altura_jogo, fonte_padrao, FPS):
         self.largura_jogo = largura_jogo
         self.altura_jogo = altura_jogo
         self.font_texto = pygame.font.Font(fonte_padrao, 18)
         self.image_fundo = pygame.image.load('assets/img/starfield.png') # Carrega uma imagem
-        self.tamanho_fundo = (largura_jogo, altura_jogo)
+        self.tamanho_fundo = np.array([largura_jogo, altura_jogo])
+        self.fps = FPS
     
 
     def atualiza_estado(self):
@@ -107,18 +100,80 @@ class TelaInicial:
         pygame.display.update()
 
 
+class telaJogo:
+    def __init__(self, largura_jogo, altura_jogo, fonte_padrao, FPS, s0, v0, tamanho_bola, tamanho_torre, posicao_torre, tamanho_canhao, posicao_canhao, posicao_atrator, raio_atrator, gravidade_atrator, tamanho_atrator):
+        self.largura_jogo = largura_jogo
+        self.altura_jogo = altura_jogo
+        self.font_texto = pygame.font.Font(fonte_padrao, 18)
+        self.image_fundo = pygame.image.load('assets/img/starfield.png') # Carrega uma imagem
+        self.tamanho_fundo = np.array([largura_jogo, altura_jogo])
+        self.fps = FPS
+        self.s0 = np.array(s0)
+        self.v0 = np.array(v0)
+        self.tamanho_bola = np.array(tamanho_bola)
+        self.tamanho_torre = np.array(tamanho_torre)
+        self.posicao_torre = np.array(posicao_torre)
+        self.tamanho_canhao = np.array(tamanho_canhao)
+        self.posicao_canhao = np.array(posicao_canhao)
+        self.posicao_atrator = np.array(posicao_atrator)
+        self.tamanho_atrator = np.array(tamanho_atrator)
+        self.raio_atrator = raio_atrator
+        self.gravidade_atrator = gravidade_atrator
+        bolinha = Bolinha(self.s0, self.v0, self.posicao_torre, self.tamanho_bola)
+        torre = Torre(self.tamanho_torre, self.posicao_torre)
+        canhao = Canhao(self.tamanho_canhao, self.posicao_canhao)
+        atrator = Atrator(self.posicao_atrator, self.raio_atrator, self.gravidade_atrator, self.tamanho_atrator)
+
+
+    def atualiza_estado(self):
+        for event in pygame.event.get(): # Retorna uma lista com todos os eventos que ocorreram desde a última vez que essa função foi chamada
+            if event.type == pygame.QUIT: 
+                return -1                                                                                                                                                                                       
+        
+        a = self.atrator.calcula_atracao(self.bolinha.posicoes)
+
+        self.bolinha.atualiza_estado(self.posicao_torre, a)
+
+
+    def desenha(self, window):
+        window.fill((0, 0, 0))
+
+        fundo = pygame.transform.scale(self.image_fundo, self.tamanho_fundo) # Redefinir dimensão da imagem
+        window.blit(fundo, (0, 0)) # Desenha a imagem já carregada por pygame.image.load em window na posição (x, y).
+
+        self.bolinha.desenha(window)
+
+        self.torre.desenha(window)
+
+        self.atrator.desenha(window)
+
+        self.canhao.desenha(window)
+
+
+        pygame.display.update()
+
+
 class Jogo:
     def __init__(self):
         pygame.init()
         self.largura_jogo = 550
         self.altura_jogo = 600
         self.fonte_padrao = pygame.font.get_default_font() # Carrega a fonte padrão
-        self.largura_nave = 60
-        self.altura_nave = 50
         self.window = pygame.display.set_mode((self.largura_jogo, self.altura_jogo))
-        pygame.display.set_caption('Space Atari') # Define o título da janela
+        self.fps = 60
+        self.s0 = np.array([50, 550])
+        self.v0 = np.array([-5, -5])
+        self.tamanho_bola = np.array([30, 30])
+        self.tamanho_torre = np.array([100, 100])
+        self.posicao_torre = np.array([500, 50])
+        self.tamanho_canhao = np.array([80, 80])
+        self.posicao_canhao = np.array([self.largura_jogo - self.tamanho_canhao[0], self.altura_jogo - self.tamanho_canhao[1] ])
+        self.posicao_atrator = np.array([350, 200])
+        self.tamanho_atrator = np.array([80, 80])
+        self.raio_atrator = 40
+        self.gravidade_atrator = 1000
         self.indice_tela_atual = 0
-        self.telas = [TelaInicial(self.largura_jogo, self.altura_jogo, self.fonte_padrao)]
+        self.telas = [TelaInicial(self.largura_jogo, self.altura_jogo, self.fonte_padrao), telaJogo(self.largura_jogo, self.altura_jogo, self.fonte_padrao, self.fps, self.s0, self.v0, self.tamanho_bola, self.tamanho_torre, self.posicao_torre, self.tamanho_canhao, self.posicao_canhao, self.posicao_atrator, self.raio_atrator, self.gravidade_atrator, self.tamanho_atrator)]
 
     def game_loop(self):
         tela_atual = self.telas[self.indice_tela_atual]
