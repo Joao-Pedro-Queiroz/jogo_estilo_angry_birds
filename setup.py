adab945a58e33afaf392bc9f46478111d8fc9bf0
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-08-16 17:58:00 -0300

    alterando alguns nomes e README

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="angrybirdspace",
+    name="spatial_collapse",
     version="0.1.0",
     author="João Gabriel Faus Faraco, João Pedro Queiroz Viana",
     author_email="joaogff1@al.insper.edu, joaopqv@al.insper.edu.br",
     description="Uma recriação do Angry Birds Space em Pygame.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/RicardolCarvalho/AngryBirdSpace",
+    url="https://github.com/Joao-Pedro-Queiroz/spatial_collapse",
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
     classifiers=[
