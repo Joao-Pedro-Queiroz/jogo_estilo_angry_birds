e0e4abca3b53df8bd5c905f7d7436dd005910fb8
Joao-Gabriel05 <jgffaraco@gmail.com>
2024-08-20 11:48:15 -0300

    pip install feito

--- spatial_collapse/jogo.py
+++ spatial_collapse/jogo.py
@@ -1,6 +1,5 @@
-from Classes import Jogo
-
-
-j = Jogo()
-Jogo.atual = j
-j.game_loop()
+from spatial_collapse.Classes import Jogo
+def main():
+    j = Jogo()
+    Jogo.atual = j
+    j.game_loop()
