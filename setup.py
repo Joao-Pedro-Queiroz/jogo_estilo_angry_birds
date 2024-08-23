a6a905bd197d9a020d004b11591731b1a52ccfa6
Joao-Gabriel05 <jgffaraco@gmail.com>
2024-08-23 10:51:57 -0300

    teste setup

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name="spatial_collapse",
     version="0.1.0",
     packages=find_packages(),
-    install_requires = [
-    ],
+    install_requires=[],
+    include_package_data=True,  # Isso permite que os dados especificados no MANIFEST.in sejam incluídos
     package_data={
-        '': ['img/*.png', 'spatial_collapse/assets/img/*.png',]
+        'spatial_collapse': ['assets/img/*.png'],  # Caminho relativo ao pacote
     },
-  entry_points={
+    entry_points={
         'console_scripts': [
             'spatial_collapse=spatial_collapse.jogo:main',
         ],
@@ -27,3 +27,36 @@ setup(
     ],
     python_requires='>=3.6',
 )
+
+
+
+
+# from setuptools import setup, find_packages
+
+# setup(
+#     name="spatial_collapse",
+#     version="0.1.0",
+#     packages=find_packages(),
+#     install_requires = [
+#     ],
+#     package_data={
+#         '': ['img/*.png', 'spatial_collapse/assets/img/*.png',]
+#     },
+#   entry_points={
+#         'console_scripts': [
+#             'spatial_collapse=spatial_collapse.jogo:main',
+#         ],
+#     },
+#     author="João Gabriel Faus Faraco, João Pedro Queiroz Viana",
+#     author_email="joaogff1@al.insper.edu, joaopqv@al.insper.edu.br",
+#     description="Uma recriação do Angry Birds Space em Pygame.",
+#     long_description=open('README.md').read(),
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/Joao-Pedro-Queiroz/spatial_collapse",
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "License :: OSI Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ],
+#     python_requires='>=3.6',
+# )
